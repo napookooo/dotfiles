3cec51f365cb07428ecd09361bb7e4c0c198fee6
cuzoF <90764385+napookooo@users.noreply.github.com>
2024-04-22 14:53:34 +0000

    Fix bugs.

--- install_scripts.sh
+++ install_scripts.sh
@@ -6,7 +6,7 @@
 echo "Install script for cuzof config files (Hyprland, rofi, waybar, fcitx5)"
 
 echo "Install dependencies for arch-base distro (assuming that using yay as AUR helper)? (Y/n) "
-read -p confirm
+read confirm
 if [[ "$confirm" == "Y" ]] || [[ "$confirm" == "y" ]] || [[ "$confirm" == "" ]] ; then
   yay -Sy
   yay -S Hyprland rofi hyprpaper swaync waybar fcitx5 swayidle polkit-kde-agent cliphist brightnessctl swaylock-effects-git kitty thunar --needed
@@ -15,7 +15,7 @@ else
 fi
 
 echo "Move all config files to your .config folders? (Y/n) "
-read -p confirm
+read confirm
 if [[ "$confirm" == "Y" ]] || [[ "$confirm" == "y" ]] || [[ "$confirm" == "" ]] ; then
   cp -r ./* ~/.config/
 else
@@ -23,7 +23,7 @@ else
 fi
 
 echo "Install fcitx5 themes? (Y/n) "
-read -p confirm
+read confirm
 if [[ "$confirm" == "Y" ]] || [[ "$confirm" == "y" ]] || [[ "$confirm" == "" ]] ; then
   ./installScripts/fcitx5
 else
